581bf92e692f8ac2ecbc7e8f505b7758aa7914f3
HuyDNA <huydo862003@gmail.com>
2025-01-17 00:16:55 +0700

    chore: init flake & uv

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          uv
+          ruff
+          python312
+          python312Packages.pygame
+        ];
+        shellHook = ''
+          uv sync
+        '';
+      };
+    });
+}
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from zombie-smash!")
+
+
+if __name__ == "__main__":
+    main()
